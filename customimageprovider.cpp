2d61c261f382f8ff97c31d589fda8df2369840d4
George Florea Bănuș <georgefb899@gmail.com>
2025-07-27 18:38:35 +0300

    use runnable

--- customimageprovider.cpp
+++ customimageprovider.cpp
@@ -1,11 +1,11 @@
 #include "customimageprovider.h"
 
 #include <QTransform>
+#include <QThreadPool>
 
 CustomImageProvider::CustomImageProvider()
     : QQuickAsyncImageProvider()
 {
-
 }
 
 QQuickImageResponse *CustomImageProvider::requestImageResponse(const QString &id, const QSize &requestedSize)
@@ -16,15 +16,31 @@ QQuickImageResponse *CustomImageProvider::requestImageResponse(const QString &id
 
 CustomImageResponse::CustomImageResponse(const QString &id, const QSize &requestedSize)
 {
-    QImage image{id};
-    QImage scaled = image.scaled(requestedSize);
-    QImage rotated = scaled.transformed(QTransform().rotate(180.0));
-    m_image = rotated;
+    auto runnable = new AsyncImageResponseRunnable(id, requestedSize);
+    connect(runnable, &AsyncImageResponseRunnable::done, this, [this](QImage image) {
+        m_image = image;
+        Q_EMIT finished();
+    });
 
-    Q_EMIT finished();
+    QThreadPool::globalInstance()->start(runnable);
 }
 
 QQuickTextureFactory *CustomImageResponse::textureFactory() const
 {
     return QQuickTextureFactory::textureFactoryForImage(m_image);
+}
+
+AsyncImageResponseRunnable::AsyncImageResponseRunnable(const QString &id, const QSize &requestedSize)
+    : m_id{id}
+    , m_requestedSize{requestedSize}
+{
+}
+
+void AsyncImageResponseRunnable::run()
+{
+    QImage image{m_id};
+    QImage scaled = image.scaled(m_requestedSize);
+    QImage rotatedImage = scaled.transformed(QTransform().rotate(180.0));
+
+    Q_EMIT done(rotatedImage);
 }
--- customimageprovider.h
+++ customimageprovider.h
@@ -2,6 +2,7 @@
 #define CUSTOMIMAGEPROVIDER_H
 
 #include <QQuickAsyncImageProvider>
+#include <QRunnable>
 
 class CustomImageProvider : public QQuickAsyncImageProvider
 {
@@ -14,11 +15,26 @@ class CustomImageResponse : public QQuickImageResponse
 {
 public:
     CustomImageResponse(const QString &id, const QSize &requestedSize);
-
     QQuickTextureFactory *textureFactory() const override;
 
 private:
     QImage m_image;
+};
+
+class AsyncImageResponseRunnable : public QObject, public QRunnable
+{
+    Q_OBJECT
+
+public:
+    AsyncImageResponseRunnable(const QString &id, const QSize &requestedSize);
+    void run() override;
+
+Q_SIGNALS:
+    void done(QImage image);
+
+private:
+    QString m_id;
+    QSize m_requestedSize;
 };
 
 #endif // CUSTOMIMAGEPROVIDER_H
